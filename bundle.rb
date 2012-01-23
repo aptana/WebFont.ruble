9ba563d219ddc73fa880f98912f62ab79ffaf11c
Chris Williams <cwilliams@appcelerator.com>
2012-01-23 15:06:02 -0500

    Implement APSTUD-4238 Externalize UI strings in WebFont ruble

--- bundle.rb
+++ bundle.rb
@@ -2,18 +2,19 @@ require 'ruble'
 
 bundle do |bundle|
   bundle.author = 'Andrew Shebanow'
-  bundle.display_name = 'WebFont'
+  bundle.display_name = t(:bundle_name)
   bundle.description = "Support for WebFont Loader API from google & typekit, see https://code.google.com/apis/webfonts/docs/webfont_loader.html"
   bundle.copyright = "Copryright 2010 Aptana. Distributed under MIT license."
+  bundle.repository = "git://github.com/aptana/wordpress.ruble.git"
 
-  bundle.menu "WebFont" do |menu|    
-    menu.command "Insert webfont.js <script> tag"
+  bundle.menu t(:bundle_name) do |menu|
+    menu.command t(:insert_webfont_script_tag)
     menu.separator
-    menu.command "Insert WebFont.load section (google)"
-    menu.command "Insert WebFont.load section (TypeKit)"
-    menu.command "Insert WebFont.load section (custom)"
+    menu.command t(:insert_webfont_load_google)
+    menu.command t(:insert_webfont_load_typekit)
+    menu.command t(:insert_webfont_load_custom)
     menu.separator
-    menu.command "Insert .wf-active Style"
-    menu.command "Insert .wf-inactive Style"
+    menu.command t(:insert_active_style)
+    menu.command t(:insert_inactive_style)
   end
 end
--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -1,68 +1,66 @@
 require 'ruble'
 
-snippet 'Insert webfont.js <script> tag' do |s|
-  s.input = :none
-  s.output = :insert_as_snippet
-  s.trigger = 'wfjs'
-  s.scope = 'text.html'
-  s.expansion = '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js">'
-end
+with_defaults :scope => 'text.html' do
+  snippet t(:insert_webfont_script_tag) do |s|
+    s.input = :none
+    s.output = :insert_as_snippet
+    s.trigger = 'wfjs'
+    s.expansion = '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js">'
+  end
 
-snippet 'Insert WebFont.load section (google)' do |s|
-  s.input = :none
-  s.output = :insert_as_snippet
-  s.trigger = 'wfloadg'
-  s.scope = 'text.html'
-  s.expansion = '<script type="text/javascript">
+  snippet t(:insert_webfont_load_google) do |s|
+    s.input = :none
+    s.output = :insert_as_snippet
+    s.trigger = 'wfloadg'
+    s.expansion = '<script type="text/javascript">
   WebFont.load({
     google: {
       families: [ "${1:Tangerine}", "${2:Cantarell}" ]
     }
   });
 </script>'
-end
+  end
 
-snippet 'Insert WebFont.load section (TypeKit)' do |s|
-  s.input = :none
-  s.output = :insert_as_snippet
-  s.trigger = 'wfloadtk'
-  s.scope = 'text.html'
-  s.expansion = '<script type="text/javascript">
+  snippet t(:insert_webfont_load_typekit) do |s|
+    s.input = :none
+    s.output = :insert_as_snippet
+    s.trigger = 'wfloadtk'
+    s.expansion = '<script type="text/javascript">
   WebFont.load({
     typekit: "${1:MY_TYPEKIT_FONTID}"
   });
 </script>'
-end
+  end
 
-snippet 'Insert WebFont.load section (custom)' do |s|
-  s.input = :none
-  s.output = :insert_as_snippet
-  s.trigger = 'wfloadc'
-  s.scope = 'text.html'
-  s.expansion = '<script type="text/javascript">
+  snippet t(:insert_webfont_load_custom) do |s|
+    s.input = :none
+    s.output = :insert_as_snippet
+    s.trigger = 'wfloadc'
+    s.expansion = '<script type="text/javascript">
   WebFont.load({
     families: [ "${1:My Font}", "${2:My Other Font}" ] 
     urls: ["${3:/fonts.css}"]
   });
 </script>'
+  end
 end
 
-snippet 'Insert .wf-active Style' do |s|
-  s.input = :none
-  s.output = :insert_as_snippet
-  s.trigger = 'wfa'
-  s.scope = 'source.css'
-  s.expansion = '.wf-active ${1:p} {
+with_defaults :scope => 'source.css' do
+  snippet t(:insert_active_style) do |s|
+    s.input = :none
+    s.output = :insert_as_snippet
+    s.trigger = 'wfa'
+    s.expansion = '.wf-active ${1:p} {
   font-family: "${2:My Font}", "${3:serif}";
 }'
-end
-
-snippet 'Insert .wf-inactive Style' do |s|
-  s.input = :none
-  s.output = :insert_as_snippet
-  s.trigger = 'wfi'
-  s.scope = 'source.css'
-  s.expansion = '.wf-inactive ${1:p} {
+  end
+  
+  snippet t(:insert_inactive_style) do |s|
+    s.input = :none
+    s.output = :insert_as_snippet
+    s.trigger = 'wfi'
+    s.expansion = '.wf-inactive ${1:p} {
   font-family: "${2:serif}";
 }'
+  end
 end
